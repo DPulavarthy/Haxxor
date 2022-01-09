41e4f18efdba1d88ab4617954b33a60fd8129a37
Kurasad <pulavarthy.dhanush@gmail.com>
2022-01-09 04:44:22 -0600

    dev: Testing new features

--- src/haxxor.ts
+++ src/haxxor.ts
@@ -1,3 +1,58 @@
-import { Preload } from '#manager'
+// import { config } from 'dotenv'
+// import { Client, Options } from 'discord.js'
 
-new Preload()
+// if (config()?.error) throw new ReferenceError('Failed to load .env properties.')
+
+// const client: Client = new Client({ intents: ['GUILD_MESSAGES'] })
+
+// client.on('ready', _ => {
+//     console.log('Ready')
+// })
+
+// client.login(process.env.TOKEN)
+
+// process.on('SIGINT', e => {
+//     client?.destroy()
+//     process.exit(1)
+// })
+
+// process.on('uncaughtException', error => {
+//     console.log(error)
+//     process.exit(1)
+// })
+
+
+// Import modules.
+import { Preload } from '#manager'
+import { config } from 'dotenv'
+import { Client, Options } from 'discord.js'
+
+config()
+
+/**
+ * Create and login to Discord client with custom properties.
+ * 
+ * @param [] None required.
+ * @example new Haxxor()
+ */
+export default class Haxxor extends Client {
+    public constructor() {
+        super({
+            restTimeOffset: 100,
+            intents: ['GUILDS', 'GUILD_MESSAGES'],
+            makeCache: Options.cacheWithLimits({ MessageManager: { maxSize: 200, sweepInterval: 5 * 60000 } })
+        })
+
+        // Login to client.
+        // this.login(process.env.TOKEN).catch(error => { throw new ReferenceError(error) })
+    }
+}
+
+// Globally define client.
+// new Haxxor()
+
+(async () => {
+    let x = await Preload.run.catch(e => e)
+
+    console.log(x)
+})()
